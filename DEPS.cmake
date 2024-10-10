0935da374da362f56b2213bf540b3ca634fc3166
coolkiid <73600915+coolkiid@users.noreply.github.com>
2024-10-10 19:38:29 +0800

    chore: optimize publish workflow
    1. optimize publish_helper.py script.
    2. add gn.ensure.
    3. add DEPS.cmake.

---
+++ DEPS.cmake
@@ -0,0 +1,12 @@
+deps = {
+    "buildtools-cmake-darwin.tar.gz": {
+        "type": "http",
+        "url": "https://dl.google.com/android/repository/ba34c321f92f6e6fd696c8354c262c122f56abf8.cmake-3.18.1-darwin.zip",
+        "decompress": False,
+    },
+    "buildtools-cmake-linux.tar.gz": {
+        "type": "http",
+        "url": "https://cmake.org/files/v3.18/cmake-3.18.1-Linux-x86_64.tar.gz",
+        "decompress": False,
+    },
+}
